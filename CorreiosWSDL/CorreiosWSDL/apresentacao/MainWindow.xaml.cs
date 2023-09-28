c95baffbe1df78e793a7f3e964ae7908cfae9299
Reverdan Almeida Sparinger <reverdan@gmail.com>
2023-09-27 21:46:34 -0300

    WSDL Correios
    Consumo de web service com WCF

---
+++ CorreiosWSDL/CorreiosWSDL/apresentacao/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using CorreiosWSDL.modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CorreiosWSDL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, RoutedEventArgs e)
+        {
+            Controle controle = new Controle();
+            Endereco endereco = controle.pesquisarEndereco(txbCep.Text);
+            if (controle.mensagem.Equals(""))
+            {
+                txbLogradouro.Text = endereco.logradouro;
+                txbBairro.Text = endereco.bairro;
+                txbCidade.Text = endereco.cidade;
+            }
+            else
+            {
+                MessageBox.Show(controle.mensagem);
+            }
+
+            
+
+        }
+    }
+}
---
+++ CorreiosWSDL/CorreiosWSDL/modelo/Controle.cs
@@ -0,0 +1,33 @@
+﻿using ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorreiosWSDL.modelo
+{
+    public class Controle
+    {
+        public String mensagem;
+
+        public Endereco pesquisarEndereco(String cep)
+        {
+            this.mensagem = "";
+            AtendeClienteClient atendeCliente = new();
+            Endereco endereco = new();
+            try
+            {
+                Task<consultaCEPResponse> address = atendeCliente.consultaCEPAsync(cep);
+                endereco.logradouro = address.Result.@return.end;
+                endereco.bairro = address.Result.@return.bairro;
+                endereco.cidade = address.Result.@return.cidade;
+            }
+            catch (System.Exception e)
+            {
+                this.mensagem = "CEP inválido";
+            }
+            return endereco;
+        }
+    }
+}
