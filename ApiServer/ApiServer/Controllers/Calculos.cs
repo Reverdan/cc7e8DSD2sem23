5c9af931d92d259e0b2d09d117b5122fd77a42f2
Reverdan Almeida Sparinger <reverdan@gmail.com>
2023-10-18 21:31:42 -0300

    Api server e client

---
+++ ApiServer/ApiServer/Controllers/Calculos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Calculos : ControllerBase
+    {
+        [HttpGet("Somar")]
+        public String somar(Int16 numero1, Int16 numero2)
+        {
+            return (numero1 + numero2).ToString();
+        }
+
+        [HttpGet("Subtrair")]
+        public String subtrair(Int16 numero1, Int16 numero2)
+        {
+            return (numero1 - numero2).ToString();
+        }
+    }
+}
